2b86daad60986b7dc13abeed046431432e795394
luisluft <luberluft@gmail.com>
2021-06-06 16:12:03 -0400

    refactor code to create new helper function setAttributes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Helper functions
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 let photosArray = [];
@@ -7,14 +14,18 @@ function displayPhotos() {
   photosArray.forEach((photo) => {
     // Create <a> link to unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
 
     // create <img> for the photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
     // Insert the img into the a element
     item.appendChild(img);
@@ -24,7 +35,7 @@ function displayPhotos() {
 }
 
 // Unsplash API
-const count = 30;
+const count = 10;
 const apiKey = "HtG8M0iPzyC9YIQCF0KOhe6gOmRWqjd5Cyx6tB5JDU8";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
