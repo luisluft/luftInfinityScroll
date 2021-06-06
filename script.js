0f6a3754fdcce380fa687e907b65cc45630fcecb
luisluft <luberluft@gmail.com>
2021-06-06 16:06:43 -0400

    add feature to display the photos on the page

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+let photosArray = [];
+
+function displayPhotos() {
+  console.log(photosArray);
+  photosArray.forEach((photo) => {
+    // Create <a> link to unsplash
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+
+    // create <img> for the photo
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+
+    // Insert the img into the a element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+    console.log(item);
+  });
+}
+
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = "HtG8M0iPzyC9YIQCF0KOhe6gOmRWqjd5Cyx6tB5JDU8";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 async function getPhotosFromApi() {
   try {
+    //   Get the photos from the API
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
   } catch (error) {}
 }
 
